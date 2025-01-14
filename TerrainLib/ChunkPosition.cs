fb4c5ecb9a40de5cc5c2bf242c51b682bb47929d
will11600 <wb11600@gmail.com>
2025-01-14 00:29:22 +0000

    Refactor ChunkPosition<T> constructor and Decode method
    Modified the ChunkPosition<T> struct by removing the original
    primary constructor and adding a new explicit constructor with
    parameters (uint x, uint y, TerrainChunk<T> chunk). The fields
    position and chunk are now initialized within the new constructor.
    Updated the Decode method to use the new constructor syntax
    new ChunkPosition<T>(x, z, TerrainChunk<T>.Decode(chunkAndVertexData,
    ref biomes)) instead of the previous shorthand.

--- TerrainLib/ChunkPosition.cs
+++ TerrainLib/ChunkPosition.cs
@@ -1,13 +1,19 @@
 ﻿namespace TinyTerrain
 {
-    internal struct ChunkPosition<T>(uint x, uint y, TerrainChunk<T> chunk) where T : BiomeSettings
+    internal struct ChunkPosition<T> where T : BiomeSettings
     {
-        public Vector2UInt position = new(x, y);
-        public TerrainChunk<T> chunk = chunk;
+        public Vector2UInt position;
+        public TerrainChunk<T> chunk;
+
+        public ChunkPosition(uint x, uint y, TerrainChunk<T> chunk)
+        {
+            position = new Vector2UInt(x, y);
+            this.chunk = chunk;
+        }
 
         public static ChunkPosition<T> Decode(ReadOnlySpan<byte> chunkAndVertexData, ref IBiome<T>[] biomes, uint x, uint z)
         {
-            return new(x, z, TerrainChunk<T>.Decode(chunkAndVertexData, ref biomes));
+            return new ChunkPosition<T>(x, z, TerrainChunk<T>.Decode(chunkAndVertexData, ref biomes));
         }
     }
 }
