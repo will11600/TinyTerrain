8e01cdaa28976ba6ace344046c8ef061500df7f6
will11600 <wb11600@gmail.com>
2025-01-14 01:09:18 +0000

    Refactor TerrainStreamingHandler initialization
    - Initialize `position` to `Vector2.Zero` and set `IsDirty` to `true` in the `TerrainStreamingHandler(byte radius)` constructor.
    - Use `new Vector2` constructor for `topLeft` in `GetTopLeftOfRange`.
    - Use `new Vector2` constructor for `bottomRight` in `GetBottomRightOfRange`.

--- TerrainLib/TerrainStreamingHandler.cs
+++ TerrainLib/TerrainStreamingHandler.cs
@@ -43,19 +43,20 @@ namespace TinyTerrain
 
         internal TerrainStreamingHandler(byte radius)
         {
+            position = Vector2.Zero;
             IsDirty = true;
             this.radius = radius;
         }
 
         internal readonly Vector2UInt GetTopLeftOfRange()
         {
-            Vector2 topLeft = new(position.X - radius, position.Y - radius);
+            Vector2 topLeft = new Vector2(position.X - radius, position.Y - radius);
             return Vector2UInt.WorldSpaceToChunkIndices(topLeft);
         }
 
         internal readonly Vector2UInt GetBottomRightOfRange()
         {
-            Vector2 bottomRight = new(Position.X + radius, Position.Y + radius);
+            Vector2 bottomRight = new Vector2(Position.X + radius, Position.Y + radius);
             return Vector2UInt.WorldSpaceToChunkIndices(bottomRight);
         }
     }
