c9315ecb8c9c57e5fe5bc63bd99ca0fb2dea8bcf
will11600 <wb11600@gmail.com>
2025-01-14 01:42:41 +0000

    Refactor TerrainChunk and update XML documentation
    Updated namespace with additional using directives. Added XML documentation to TerrainChunk constants. Refactored BiomeId and BaseHeight property setters for explicit comparison. Enhanced TerrainChunk constructor to include new parameters and conditional array allocation. Improved Decode method to utilize the new constructor and streamline vertex initialization. Updated Encode method documentation to reflect changes.

--- TerrainLib/TerrainChunk.cs
+++ TerrainLib/TerrainChunk.cs
@@ -1,4 +1,10 @@
-﻿namespace TinyTerrain
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace TinyTerrain
 {
     // 11111000 - Height
     // 00000111 - Biome ID
@@ -8,8 +14,19 @@
     /// </summary>
     public struct TerrainChunk<T> where T : BiomeSettings
     {
+        /// <summary>
+        /// The width of the chunk.
+        /// </summary>
         public const byte WIDTH = 8;
+
+        /// <summary>
+        /// The height of the chunk.
+        /// </summary>
         public const byte HEIGHT = 8;
+
+        /// <summary>
+        /// The total number of vertices in the chunk.
+        /// </summary>
         public const byte RESOLUTION = WIDTH * HEIGHT;
 
         internal const byte BUFFER_SIZE = RESOLUTION + 1;
@@ -32,7 +49,7 @@
             readonly get => biomeId;
             set
             {
-                if (value is < 0 or > 7)
+                if (value < 0 || value > 7)
                 {
                     throw new ArgumentOutOfRangeException(nameof(value), value, "The biome ID must be between 0 and 15.");
                 }
@@ -52,7 +69,7 @@
             readonly get => baseHeight;
             set
             {
-                if (value is < 0 or > 31)
+                if (value < 0 || value > 31)
                 {
                     throw new ArgumentOutOfRangeException(nameof(value), value, "The biome ID must be between 0 and 31.");
                 }
@@ -79,24 +96,28 @@
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="TerrainChunk"/> struct with an empty vertices array.
+        /// Initializes a new instance of the <see cref="TerrainChunk"/> struct.
         /// </summary>
-        public TerrainChunk()
+        /// <param name="palette">The material palette used by the chunk.</param>
+        /// <param name="baseHeight">The base height of the chunk.</param>
+        /// <param name="biomeId">The biome ID of the chunk.</param>
+        public TerrainChunk(MaterialPalette palette, byte baseHeight, byte biomeId)
         {
+            this.palette = palette;
+            this.baseHeight = baseHeight;
+            this.biomeId = biomeId;
+            #if NET8_0_OR_GREATER
+            vertices = GC.AllocateUninitializedArray<TerrainVertex<T>>(RESOLUTION);
+            #else
             vertices = new TerrainVertex<T>[RESOLUTION];
-        }
-
-        private TerrainChunk(ref TerrainVertex<T>[] vertices)
-        {
-            this.vertices = vertices;
+            #endif
         }
 
         /// <summary>
         /// Decodes the compressed chunk data and vertex data into a <see cref="TerrainChunk"/> instance.
         /// </summary>
-        /// <param name="chunkData">The compressed chunk data.</param>
         /// <param name="chunkAndVertexData">A span of compressed vertex data.</param>
-        /// <param name="biome">The biome for the chunk.</param>
+        /// <param name="biomes">The biome for the chunk.</param>
         /// <returns>The decoded <see cref="TerrainChunk"/> instance.</returns>
         /// <exception cref="ArgumentException">Thrown if the length of the <paramref name="chunkAndVertexData"/> span does not match the expected number of vertices in the chunk.</exception>
         public static TerrainChunk<T> Decode(ReadOnlySpan<byte> chunkAndVertexData, ref IBiome<T>[] biomes)
@@ -106,22 +127,16 @@
                 throw new ArgumentException($"The length of the vertices array must be {BUFFER_SIZE}.", nameof(chunkAndVertexData));
             }
 
-            TerrainVertex<T>[] terrainVertices = GC.AllocateUninitializedArray<TerrainVertex<T>>(RESOLUTION);
-
             byte chunkData = chunkAndVertexData[0];
-
+            byte baseHeight = (byte)((chunkData >> 3) & 0x1F);
             byte biomeId = (byte)(chunkData & 0x07);
+            MaterialPalette palette = biomes[biomeId].Palette;
 
-            TerrainChunk<T> chunk = new(ref terrainVertices)
-            {
-                palette = biomes[biomeId].Palette,
-                baseHeight = (byte)((chunkData >> 3) & 0x1F),
-                biomeId = biomeId
-            };
+            TerrainChunk<T> chunk = new TerrainChunk<T>(palette, baseHeight, biomeId);
 
-            for (int i = 1; i < chunk.Length; i++)
+            for (int i = 1; i < RESOLUTION; i++)
             {
-                terrainVertices[i] = TerrainVertex<T>.Decode(chunkAndVertexData[i], chunk);
+                chunk.vertices[i] = TerrainVertex<T>.Decode(chunkAndVertexData[i], chunk);
             }
 
             return chunk;
@@ -131,7 +146,6 @@
         /// Encodes the terrain chunk data into a sequence of bytes and writes them to the buffer.
         /// </summary>
         /// <param name="buffer">The buffer to write the encoded data to.</param>
-        /// <param name="biomeId">The ID of the biome.</param>
         /// <exception cref="ArgumentException">Thrown if the buffer size is not equal to <see cref="BUFFER_SIZE"/>.</exception>
         public readonly void Encode(Span<byte> buffer)
         {
